36a9801a43e0023c3f1983502b67e0490dffa00b
Firingo <petr.havlin.0605@gmail.com>
2022-07-27 19:11:48 +0200

    Pokročilá úloha - výpočet kruhu

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Zadej poloměr kruhu (cm):");
+float a = float.Parse (Console.ReadLine());
+
+float obvod = 2 * 3.1415f * a;
+float obsah = 3.1415f * a * a;
+
+Console.WriteLine("Obvod zadaného kruhu je: " + obvod + " cm");
+Console.WriteLine("Jeho obsah je "  + obsah + " cm^2");
+Console.ReadKey();
